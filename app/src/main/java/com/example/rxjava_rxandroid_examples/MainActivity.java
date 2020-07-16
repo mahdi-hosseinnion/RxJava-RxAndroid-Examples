cd4df71d416f1a2585c0e13b0026f9cb7be29329
seyed mahdi <ssmmhh1382@gmail.com>
2020-07-16 10:43:48 +0100

    02-Observables and observers with RxJava and RxAndroid

--- app/src/main/java/com/example/rxjava_rxandroid_examples/MainActivity.java
+++ app/src/main/java/com/example/rxjava_rxandroid_examples/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.rxjava_rxandroid_examples;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Observable<Task> taskObservable= Observable
+                .fromIterable(DataSource.createTasksList())
+                .subscribeOn(Schedulers.io())
+                .filter(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) throws Exception {
+                        Log.d(TAG, "test: called");
+                        Log.d(TAG, "test: "+Thread.currentThread().getName());
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return task.isComplete();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe: Called");
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Log.d(TAG, "onNext: "+Thread.currentThread().getName());
+                Log.d(TAG, "onNext: "+task.getDescription());
+                Log.d(TAG, "onNext: "+task.isComplete());
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: ", e);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: called");
+            }
+        });
     }
 }
