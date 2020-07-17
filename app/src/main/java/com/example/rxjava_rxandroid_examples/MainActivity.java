8dc89ba5d17be184633116fe7235b87e709c3759
seyed mahdi <ssmmhh1382@gmail.com>
2020-07-17 14:46:17 +0100

    06-create, just, range, repeat

--- app/src/main/java/com/example/rxjava_rxandroid_examples/MainActivity.java
+++ app/src/main/java/com/example/rxjava_rxandroid_examples/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.rxjava_rxandroid_examples;
 
 import androidx.appcompat.app.AppCompatActivity;
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
@@ -25,52 +28,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Observable<Task> taskObservable= Observable
-                .fromIterable(DataSource.createTasksList())
-                .subscribeOn(Schedulers.io())
-                .filter(new Predicate<Task>() {
+                .range(1,10)
+                .repeat(5)
+                .filter(new Predicate<Integer>() {
                     @Override
-                    public boolean test(Task task) throws Exception {
-                        Log.d(TAG, "test: called");
-                        Log.d(TAG, "test: "+Thread.currentThread().getName());
-                        try {
-                            Thread.sleep(2000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        return task.isComplete();
+                    public boolean test(Integer integer) throws Exception {
+                        return integer%2==0;
                     }
                 })
-                .observeOn(AndroidSchedulers.mainThread());
+                .map(new Function<Integer, Task>() {
+                    @Override
+                    public Task apply(Integer integer) throws Exception {
 
+                        return new Task("it is the task #",false,integer);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
         taskObservable.subscribe(new Observer<Task>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: Called");
-                disposable.add(d);
+
             }
 
             @Override
             public void onNext(Task task) {
-                Log.d(TAG, "onNext: "+Thread.currentThread().getName());
-                Log.d(TAG, "onNext: "+task.getDescription());
-                Log.d(TAG, "onNext: "+task.isComplete());
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
+                Log.d(TAG, "onNext: "+task.getDescription()+task.getPriority());
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.e(TAG, "onError: ", e);
+
             }
 
             @Override
             public void onComplete() {
-                Log.d(TAG, "onComplete: called");
+
             }
         });
+
     }
 
     @Override
