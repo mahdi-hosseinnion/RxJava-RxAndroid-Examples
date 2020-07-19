3259ea45d9116bdd7fdd507ff2bac46b168621ef
seyed mahdi <ssmmhh1382@gmail.com>
2020-07-19 16:40:10 +0100

    18-throttleFirst operator

--- app/src/main/java/com/example/rxjava_rxandroid_examples/MainActivity.java
+++ app/src/main/java/com/example/rxjava_rxandroid_examples/MainActivity.java
@@ -38,39 +38,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        searchView = findViewById(R.id.search_view);
+//        searchView = findViewById(R.id.search_view);
         timeSinceLastQuery=System.currentTimeMillis();
-        Observable<String> observableQueryText=Observable
-                .create(new ObservableOnSubscribe<String>() {
-                    @Override
-                    public void subscribe(ObservableEmitter<String> emitter) throws Exception {
-                        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-                            @Override
-                            public boolean onQueryTextSubmit(String query) {
-                                return false;
-                            }
-
-                            @Override
-                            public boolean onQueryTextChange(String newText) {
-                                if (!emitter.isDisposed())
-                                    emitter.onNext(newText);
-                                return false;
-                            }
-                        });
-                    }
-                })
-                .debounce(500,TimeUnit.MILLISECONDS)
-                .subscribeOn(Schedulers.io());
-        observableQueryText.subscribe(new Observer<String>() {
+        Observable<Unit> observableQueryText=RxView.clicks(findViewById(R.id.btn))
+                .throttleFirst(1,TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread());
+        observableQueryText.subscribe(new Observer<Unit>() {
             @Override
             public void onSubscribe(Disposable d) {
                 disposable.add(d);
             }
 
             @Override
-            public void onNext(String s) {
-                Log.d(TAG, "onNext: it has been "+(System.currentTimeMillis()-timeSinceLastQuery)+" milisecond since last query");
-                Log.d(TAG, "onNext: query = "+s);
+            public void onNext(Unit s) {
+                Log.d(TAG, "onNext: it has been "+(System.currentTimeMillis()-timeSinceLastQuery)+" milisecond since last runned");
+//                Log.d(TAG, "onNext: query = "+s);
                 timeSinceLastQuery=System.currentTimeMillis();
             }
 
