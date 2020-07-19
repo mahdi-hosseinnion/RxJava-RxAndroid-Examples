259fd2238108a72c303e36bd0cd69bfbed50e9d0
seyed mahdi <ssmmhh1382@gmail.com>
2020-07-19 15:05:35 +0100

    17-Debounce operator

--- app/src/main/java/com/example/rxjava_rxandroid_examples/MainActivity.java
+++ app/src/main/java/com/example/rxjava_rxandroid_examples/MainActivity.java
@@ -3,65 +3,75 @@ package com.example.rxjava_rxandroid_examples;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import androidx.lifecycle.ViewModelProvider;
+import androidx.appcompat.widget.SearchView;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
-import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
+import kotlin.Unit;
 
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.util.Log;
-import android.widget.TextView;
+import android.widget.Button;
 
-import com.example.rxjava_rxandroid_examples.viewModels.TaskViewModel;
+import com.jakewharton.rxbinding3.view.RxView;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     //ui
-    TextView txt;
+    SearchView searchView;
 
     //vars
 
     private CompositeDisposable disposable = new CompositeDisposable();
-
+    long timeSinceLastQuery=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt = findViewById(R.id.txt);
-        Observable<String> filterObservable = Observable
-                .fromIterable(DataSource.createTasksList())
-                .map(new Function<Task, String>() {
+        searchView = findViewById(R.id.search_view);
+        timeSinceLastQuery=System.currentTimeMillis();
+        Observable<String> observableQueryText=Observable
+                .create(new ObservableOnSubscribe<String>() {
                     @Override
-                    public String apply(Task task) throws Exception {
-                        return task.getDescription()+" xxxxxxxxx";
+                    public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                            @Override
+                            public boolean onQueryTextSubmit(String query) {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onQueryTextChange(String newText) {
+                                if (!emitter.isDisposed())
+                                    emitter.onNext(newText);
+                                return false;
+                            }
+                        });
                     }
                 })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread());
-        filterObservable.subscribe(new Observer<String>() {
+                .debounce(500,TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io());
+        observableQueryText.subscribe(new Observer<String>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: called...+++===+++...");
+                disposable.add(d);
             }
 
             @Override
-            public void onNext(String description) {
-                Log.d(TAG, "onNext: " + description);
+            public void onNext(String s) {
+                Log.d(TAG, "onNext: it has been "+(System.currentTimeMillis()-timeSinceLastQuery)+" milisecond since last query");
+                Log.d(TAG, "onNext: query = "+s);
+                timeSinceLastQuery=System.currentTimeMillis();
             }
 
             @Override
@@ -74,6 +84,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
 
 
     }
